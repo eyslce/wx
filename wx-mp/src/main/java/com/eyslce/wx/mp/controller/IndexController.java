1f34c09b90d6f84e59d68a2054d5061d1fa4c79f
tiannan <tiannan@juanpi.com>
2018-01-23 13:55:41 +0800

    增加banner

--- wx-mp/src/main/java/com/eyslce/wx/mp/controller/IndexController.java
+++ wx-mp/src/main/java/com/eyslce/wx/mp/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.eyslce.wx.mp.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.eyslce.wx.commons.util.Configuration;
 import com.eyslce.wx.commons.util.HttpClient;
 import com.eyslce.wx.mp.entity.DeliveryType;
