b1e5abb763aea5ee841afc64b7315c777a363170
eyslce <holyking@sina.cn>
2018-05-16 20:14:38 +0800

    引入jquery-weui

--- wx-mp/src/main/java/com/eyslce/wx/mp/WxMpApplication.java
+++ wx-mp/src/main/java/com/eyslce/wx/mp/WxMpApplication.java
@@ -1,6 +1,7 @@
 package com.eyslce.wx.mp;
 
 import com.eyslce.wx.commons.util.WxConfigurationProperties;
+import com.eyslce.wx.mp.controller.ErrorController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,10 +9,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"com.eyslce.wx.mp", "com.eyslce.wx.commons"})
+@ComponentScan(basePackages = {"com.eyslce.wx.mp", "com.eyslce.wx.commons"}, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ErrorController.class)
+})
 @EnableConfigurationProperties(value = WxConfigurationProperties.class)
 public class WxMpApplication extends SpringBootServletInitializer{
 
