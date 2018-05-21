9e8afbf9bc80fa7bd6039508219f30c798355b07
eyslce <holyking@sina.cn>
2018-05-21 20:48:44 +0800

    引入web3j

---
+++ bc/src/main/java/com/eyslce/bc/BcApplication.java
@@ -0,0 +1,12 @@
+package com.eyslce.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcApplication.class, args);
+	}
+}
---
+++ bc/src/test/java/com/eyslce/bc/BcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eyslce.bc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
