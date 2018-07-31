96813d80ca79f74d4b2db194d133f7ab14589f0a
eyslce <holyking@sina.cn>
2018-07-31 17:25:21 +0800

    完成粉丝信息获取

--- wx-mp/src/main/java/com/eyslce/wx/mp/controller/admin/FansController.java
+++ wx-mp/src/main/java/com/eyslce/wx/mp/controller/admin/FansController.java
@@ -1,14 +1,15 @@
 package com.eyslce.wx.mp.controller.admin;
 
 import com.eyslce.wx.commons.result.HttpResult;
+import com.eyslce.wx.commons.util.Constant;
 import com.eyslce.wx.mp.dao.AccountFansDao;
 import com.eyslce.wx.mp.domain.AccountFans;
 import com.eyslce.wx.mp.query.FansQuery;
 import com.eyslce.wx.mp.service.IFansService;
+import com.eyslce.wx.mp.util.WxMpUtil;
 import com.github.pagehelper.PageInfo;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,10 +31,22 @@ public class FansController extends BaseController {
     private IFansService fansService;
     @Autowired
     private AccountFansDao fansDao;
+    @Autowired
+    private WxMpUtil wxMpUtil;
 
     @RequestMapping("index")
     public String index() {
         return "admin/fans/index";
+    }
+
+    @RequestMapping("info")
+    public String info() {
+        return "admin/fans/info";
+    }
+
+    @RequestMapping("msglist")
+    public String msglist() {
+        return "admin/fans/msglist";
     }
 
     @RequestMapping("list")
@@ -48,34 +60,35 @@ public class FansController extends BaseController {
     @RequestMapping(value = "/syncFansList")
     @ResponseBody
     public HttpResult syncAccountFansList() throws WxErrorException {
-        //todo
+        //获取最后openID
         String nextOpenid = null;
+        AccountFans lastFans = fansDao.getLastOpenId();
+        if (null != lastFans) {
+            nextOpenid = lastFans.getOpenId();
+        }
         WxMpUserList wxUserList = wxMpService.getUserService().userList(nextOpenid);
         //获取每个用户的详细信息
         List<AccountFans> fansList = new ArrayList<AccountFans>();
         List<String> openIds = wxUserList.getOpenids();
         for (String openId : openIds) {
-            String lang = "zh_CN"; //语言
-            WxMpUser user = wxMpService.getUserService().userInfo(openId, lang);
-            AccountFans fans = new AccountFans();
-            fans.setOpenId(openId);
-            if (user.getSubscribe()) {
-                fans.setSubscribeStatus(1);
-                fans.setSubscribeTime(new Date(user.getSubscribeTime()));
-            } else {
-                fans.setSubscribeStatus(0);
+            AccountFans fans = wxMpUtil.getFansInfo(openId);
+            if (fans == null) {
+                continue;
             }
-            fans.setNicknameStr(user.getNickname());
-            fans.setGender(user.getSexId());
-            fans.setLanguage(user.getLanguage());
-            fans.setCountry(user.getCountry());
-            fans.setCity(user.getCity());
-            fans.setHeadimgurl(user.getHeadImgUrl());
-            fans.setRemark(user.getRemark());
-            fans.setStatus(1);
             fansList.add(fans);
         }
         fansDao.addList(fansList);
         return success();
+    }
+
+    @RequestMapping(value = "/syncFans")
+    @ResponseBody
+    public HttpResult syncAccountFans(String openId) throws WxErrorException {
+        AccountFans fans = wxMpUtil.getFansInfo(openId);
+        if (fans == null) {
+            return error("没有找到该用户信息");
+        }
+        fansDao.update(fans);
+        return success(fans, Constant.SUCCESS_MSG);
     }
 }
--- wx-mp/src/main/java/com/eyslce/wx/mp/util/WxMpUtil.java
+++ wx-mp/src/main/java/com/eyslce/wx/mp/util/WxMpUtil.java
@@ -3,10 +3,21 @@ package com.eyslce.wx.mp.util;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.eyslce.wx.mp.domain.Account;
+import com.eyslce.wx.mp.domain.AccountFans;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
+@Component
 public class WxMpUtil {
+
+    @Autowired
+    private WxMpService wxMpService;
 
     public static JSONObject getAccount(List<Account> list, String curentName) {
         JSONObject obj = new JSONObject();
@@ -22,5 +33,30 @@ public class WxMpUtil {
         }
         obj.put("list", arr);
         return obj;
+    }
+
+    public AccountFans getFansInfo(String openId) throws WxErrorException {
+        String lang = "zh_CN"; //语言
+        WxMpUser user = wxMpService.getUserService().userInfo(openId, lang);
+        if (user == null) {
+            return null;
+        }
+        AccountFans fans = new AccountFans();
+        fans.setOpenId(openId);
+        if (user.getSubscribe()) {
+            fans.setSubscribeStatus(1);
+            fans.setSubscribeTime(new Date(user.getSubscribeTime()));
+        } else {
+            fans.setSubscribeStatus(0);
+        }
+        fans.setNicknameStr(user.getNickname());
+        fans.setGender(user.getSexId());
+        fans.setLanguage(user.getLanguage());
+        fans.setCountry(user.getCountry());
+        fans.setCity(user.getCity());
+        fans.setHeadimgurl(user.getHeadImgUrl());
+        fans.setRemark(user.getRemark());
+        fans.setStatus(1);
+        return fans;
     }
 }
