ec12809559050e545353324bed52242628577eed
eyslce <holyking@sina.cn>
2019-03-26 17:20:08 +0800

    修复工作台相关问题

---
+++ wx-commons/src/main/java/com/eyslce/wx/commons/util/DateTimeUtils.java
@@ -0,0 +1,5 @@
+package com.eyslce.wx.commons.util;
+
+public class DateTimeUtils {
+    public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+}
--- wx-mp/src/main/java/com/eyslce/wx/mp/config/WebMvcConfig.java
+++ wx-mp/src/main/java/com/eyslce/wx/mp/config/WebMvcConfig.java
@@ -1,9 +1,18 @@
 package com.eyslce.wx.mp.config;
 
+import com.eyslce.wx.commons.util.DateTimeUtils;
 import com.eyslce.wx.mp.interceptors.LoginInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -18,4 +27,16 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
     }
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(converter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter converter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat(DateTimeUtils.DATE_TIME));
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
 }
