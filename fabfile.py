b23923615f17effba5e24cdb6429997c11080ac2
eyslce <holyking@sina.cn>
2019-06-19 15:57:48 +0800

    修改打包，增加环境配置切换

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*-
 from fabric.api import local, env, run, put, cd
+from fabric.contrib import files
 
 env.hosts = "39.98.209.7"
 env.user = "root"
-env.key_filename = "C:\Users\eyslc\Downloads\cjj.pem"
+env.key_filename = "C:\Users\eyslc\.ssh\cjj.pem"
 
 app_name = 'wx-mp'
 app_version = '1.0-SNAPSHOT'
-
+conf_list = ['LOG_FOLDER=/data/wx-mp/logs','JAVA_OPTS=-Dspring.profiles.active=prod']
 
 def clean_package():
-    local("mvn clean package -Dmaven.test.skip=true")
+    local("mvn clean package -Dmaven.test.skip=true -P prod")
 
 
 def clean_old():
@@ -18,10 +19,14 @@ def clean_old():
     with cd("/data/"):
         run("rm -rf {}".format(app_name))
         run("rm -rf {}-{}.jar".format(app_name, app_version))
+        run("mkdir -p /data/{}/logs".format(app_name))
 
 
 def upload_package():
     put("build/{}-{}.jar".format(app_name, app_version), "/data/{}-{}.jar".format(app_name, app_version), mode=0755)
+
+def conf_set():
+    files.append("/data/{}-{}.conf".format(app_name, app_version),conf_list)
 
 
 def start_service():
@@ -32,4 +37,5 @@ def deploy():
     clean_package()
     clean_old()
     upload_package()
+    conf_set()
     start_service()
